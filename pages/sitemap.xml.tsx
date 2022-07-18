67250f2e3bccccfb836a46bf8b656edfe81419b0
Quentin Laffont <quentin@flexper.fr>
2022-07-18 12:04:31 +0200

    fix(sitemap): fix fs access forbidden

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import { NextApiResponse } from 'next';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -7,16 +6,9 @@ const Sitemap = () => {};
 export const getServerSideProps = ({ res }: { res: NextApiResponse }) => {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-  const staticPages = fs
-    .readdirSync('pages')
-    .filter((staticPage) => {
-      return !['api', '_app.tsx', '_document.tsx', '_error.tsx', '404.tsx', 'sitemap.xml.tsx', 'cv.tsx'].includes(
-        staticPage,
-      );
-    })
-    .map((staticPagePath) => {
-      return `${baseUrl}/${staticPagePath.replace('.tsx', '').replace('index', '')}`;
-    });
+  const staticPages = ['about', 'gaming', 'index', 'news', 'projects', 'tools'].map((staticPagePath) => {
+    return `${baseUrl}/${staticPagePath.replace('.tsx', '').replace('index', '')}`;
+  });
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
