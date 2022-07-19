b01f737da6eda4c99e6aaa3434701b7bfbc13615
Quentin Laffont <quentin@flexper.fr>
2022-07-19 08:23:46 +0200

    fix(seo): add default configuration

--- components/atoms/SEO.tsx
+++ components/atoms/SEO.tsx
@@ -1,13 +1,14 @@
 import { NextSeo } from 'next-seo';
 import React from 'react';
 
-import { ogTitle } from '../../services/seo/next-seo.config';
+import APP_DEFAULT_SEO, { ogTitle } from '../../services/seo/next-seo.config';
 
 export const SEO = ({ title, description = undefined }: { title: string; description?: string }) => {
   return (
     <NextSeo
-      title={ogTitle(title).title}
-      openGraph={ogTitle(title)}
+      title={ogTitle(title).title || APP_DEFAULT_SEO.title}
+      openGraph={{ ...APP_DEFAULT_SEO.openGraph, ...ogTitle(title) }}
+      twitter={APP_DEFAULT_SEO.twitter}
       description={description?.replace(/<\/?[^>]+(>|$)/gi, '')}
     />
   );
